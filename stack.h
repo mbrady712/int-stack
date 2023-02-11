d87ba7dbf608771918a0514c55e46c9071f2d7d0
Michael Brady <michaelbrady712@gmail.com>
2023-02-10 18:55:00 -0600

    Added class definition of Stack

--- stack.h
+++ stack.h
@@ -7,4 +7,17 @@ Purpose: This program creates a stack ADT.
 #ifndef STACK_STACK_H
 #define STACK_STACK_H
 
+class Stack
+{
+public:
+    int top;
+    int contents[10]; 
+
+    Stack();
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
 #endif //STACK_STACK_H
