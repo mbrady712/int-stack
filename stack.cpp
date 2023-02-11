a07934eb69c5a1b9f38a83cee1b8fa72f0ef26b9
Michael Brady <michaelbrady712@gmail.com>
2023-02-11 15:07:26 -0600

    Created push() function

--- stack.cpp
+++ stack.cpp
@@ -5,3 +5,18 @@ Purpose: This program creates a stack ADT.
 **********************/
 
 #include "stack.h"
+
+Stack::Stack(){
+    top = -1;
+}
+
+bool Stack::push(int x){
+    //Move x into stack
+    if(top < SIZE){
+        top++;
+        contents[top] = x;
+    }
+    //Return true or false based on if entered properly
+    return contents[top] == x;
+}
+
