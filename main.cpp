8779eadb65aa1adfc439febd491a1a1b69d9dae7
Michael Brady <michaelbrady712@gmail.com>
2023-02-10 18:53:24 -0600

    Added extra code to main() in order to test member functions of Stack more effectively

--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@ Purpose: This program creates a stack ADT.
 #include "main.h"
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World" << std::endl;
-
-    return 0;
+    Stack s1;
+    std::cout << "push() test: " << s1.push(10) << std::endl;
+    std::cout << "Int returned from pop(): " << s1.pop() << std::endl;
+    std::cout << "Int returned from peek(): " << s1.peek() << std::endl;
+    std::cout << "isEmpty() test: " << s1.isEmpty() << std::endl;
+    std::cout << "Contents of stack: " << std::endl;
+    for(int i = 0; i < sizeof(s1.contents)/sizeof(int); i++){
+        std::cout << s1.contents[i] << std::endl;
+    }
 }
